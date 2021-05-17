035e90fd4c11d247aff2f061b8d7b066d7f5b11e
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-05-17 11:39:22 -0500

    documentation

--- Accounts.py
+++ Accounts.py
@@ -10,21 +10,7 @@ class Accounts:
 
     def __init__(self, index, customer_id, account_no, acc_type, total_amt):
         
-        self.index = index
-        self.customer_id = customer_id
-        self.account_no = account_no
-        self.acc_type = acc_type
-        self.total_amt = total_amt
-        
-    def __eq__(self, other):
-
-        if isinstance(other,Accounts):
-            return self.account_no == other.account_no
-        else:
-            return self.account_no == other
-
-    def depositAndWithdraw(self, currency, option):
-
+        self.index = index  
         if option == 1:
             self.total_amt = self.total_amt + currency
 
@@ -36,10 +22,11 @@ class Accounts:
                 print("Insufficient balance :(")
 
     def displayBalance(self):
-        print(f"\tThe balance for {self.account_no} is : ${self.total_amt} \n")
+        print(f"\tThe balance in account  {self.account_no} is : ${self.total_amt} \n")
 
     def show_account_info(self):
         print(f"Account Number : {self.account_no}\n Type : {self.acc_type}\n Total Amount : ${self.total_amt}\n\n")
+
 
     def as_dict(self):
         return {"Customer id" : self.customer_id,"Account Number" : self.account_no, "Type" : self.acc_type, "Total Amount" : self.total_amt}
