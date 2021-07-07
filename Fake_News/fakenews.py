7c19ba2731e051c3650c590aa7c0deb6353eeccf
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-07 10:08:27 -0500

    Add files via upload

--- Fake_News/fakenews.py
+++ Fake_News/fakenews.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+chunksize = 10 ** 6
+for chunk in pd.read_csv('fake.csv', chunksize= chunksize):
+    print(chunk)
+for chunk in pd.read_csv('true.csv', chunksize= chunksize):
+    print(chunk)
