4c1a422833630affb657308c5ae0c3dbd96830b0
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-07 10:09:21 -0500

    Delete fakenews.py

--- fakenews.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-chunksize = 10 ** 6
-for chunk in pd.read_csv('fake.csv', chunksize= chunksize):
-    print(chunk)
-for chunk in pd.read_csv('true.csv', chunksize= chunksize):
-    print(chunk)
