0fcd7c76adcfc406caabd389325219a5efae073c
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-07-07 10:24:34 -0500

    Update Person.py

--- Banking_System/Person.py
+++ Banking_System/Person.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import os
 
+class Person():
+    
+    name = ''
+    address = ''
+    
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
