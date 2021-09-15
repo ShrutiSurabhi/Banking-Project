e2721e94ac70e4151f793240f5109b4cddfaa032
ShrutiSurabhi <80778530+ShrutiSurabhi@users.noreply.github.com>
2021-09-15 10:44:27 -0500

    2nd testing changes

--- Hadoop Mini Project/MapReduce/autoinc_reducer2.py
+++ Hadoop Mini Project/MapReduce/autoinc_reducer2.py
@@ -1,9 +1,19 @@
-  
 import sys
+
+cur_make_year = None
+total_num_accdt = 0
 
 for line in sys.stdin:
     line = line.rstrip('\n')[1:-1]
     tokens = line.split("'")
-    make, year, num_accdt = tokens[1], tokens[3], int(tokens[-1][2:])
-    make_year_concat = '{} {}'.format(make, year)
-    print(make_year_concat, num_accdt)
+    make_year, num_accdt = tokens[1], int(tokens[-1][2:])
+
+    if cur_make_year != make_year:
+        if cur_make_year is not None:
+            print(cur_make_year, total_num_accdt)
+        total_num_accdt = 0
+        cur_make_year = make_year
+
+    total_num_accdt += 1
+
+print(cur_make_year, total_num_accdt)
